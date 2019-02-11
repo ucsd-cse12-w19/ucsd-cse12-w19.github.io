ee010d257a2021f5b39d8e822f0b90467e8116d8
Alexandra Macedo <almacedo@eng.ucsd.edu>
2019-02-10 21:43:24 -0800

    Change some indentation

--- discussion/week5-discussion-friday-partition/ClassPartitioner.java
+++ discussion/week5-discussion-friday-partition/ClassPartitioner.java
@@ -12,62 +12,62 @@ public class ClassPartitioner implements Partitioner{
 		return randomArr;
 	}	
 
-	  public static void swap(String[] array, int i1, int i2) {
-		    String temp = array[i1];
-		    array[i1] = array[i2];
-		    array[i2] = temp;
-		  }
-	  public int partition(String[] array, int low, int high) {
-            if(low == high){
-            	return low;
-            }
-		    int pivotIndex = high - 1;
-		    String pivot = array[pivotIndex];
-		    int smallerBeforeIndex = low;
-		    int largerAfterIndex = high - 2;
-		    while(largerAfterIndex >= smallerBeforeIndex) {
-		      if(Integer.parseInt(array[smallerBeforeIndex]) > Integer.parseInt(pivot)) {
-		        swap(array, smallerBeforeIndex, largerAfterIndex);
+	public static void swap(String[] array, int i1, int i2) {
+		String temp = array[i1];
+	    array[i1] = array[i2];
+	    array[i2] = temp;
+	}
+	public int partition(String[] array, int low, int high) {
+		if(low == high){
+        	return low;
+        }
+        int pivotIndex = high - 1;
+		String pivot = array[pivotIndex];
+		int smallerBeforeIndex = low;
+		int largerAfterIndex = high - 2;
+		while(largerAfterIndex >= smallerBeforeIndex) {
+			if(Integer.parseInt(array[smallerBeforeIndex]) > Integer.parseInt(pivot)) {
+				swap(array, smallerBeforeIndex, largerAfterIndex);
 		        largerAfterIndex -= 1;
-		      }
-		      else {
+		    }
+		    else {
 		        smallerBeforeIndex += 1;
-		      }
 		    }
+		}
 		    
-		    if(Integer.parseInt(array[smallerBeforeIndex]) < Integer.parseInt(pivot)){
-		    	swap(array, smallerBeforeIndex + 1, pivotIndex);
-			    System.out.println("array after: " + Arrays.toString(array));
-				System.out.println("pivot moved to index " + (smallerBeforeIndex + 1) + "\n");
-		    	return smallerBeforeIndex + 1;
-		    }
-		    else{
-		    	swap(array, smallerBeforeIndex, pivotIndex);
-			    System.out.println("array after: " + Arrays.toString(array));
-				System.out.println("pivot moved to index " + (smallerBeforeIndex) + "\n");
-		    	return smallerBeforeIndex;
-		    }
-	  }
+		if(Integer.parseInt(array[smallerBeforeIndex]) < Integer.parseInt(pivot)){
+		   	swap(array, smallerBeforeIndex + 1, pivotIndex);
+		   	System.out.println("array after: " + Arrays.toString(array));
+		   	System.out.println("pivot moved to index " + (smallerBeforeIndex + 1) + "\n");
+		   	return smallerBeforeIndex + 1;
+		}
+		else{
+		   	swap(array, smallerBeforeIndex, pivotIndex);
+		   	System.out.println("array after: " + Arrays.toString(array));
+		   	System.out.println("pivot moved to index " + (smallerBeforeIndex) + "\n");
+		   	return smallerBeforeIndex;
+		}
+	}
 	
-	  public static void main(String[] args){
-				ClassPartitioner partitioner = new ClassPartitioner();
-				String[] smallArr = {"4", "9", "-3", "4", "2", "1"}; 
-				String[] repeatedArr = {"7", "7", "7", "7", "7", "7", "7", "7", "7", "7"};
-				String[] singleElementArr = {"-2"};
+	public static void main(String[] args){
+		ClassPartitioner partitioner = new ClassPartitioner();
+		String[] smallArr = {"4", "9", "-3", "4", "2", "1"}; 
+		String[] repeatedArr = {"7", "7", "7", "7", "7", "7", "7", "7", "7", "7"};
+		String[] singleElementArr = {"-2"};
 				
-				System.out.println("small array before: " + Arrays.toString(smallArr));
-				partitioner.partition(smallArr, 0, smallArr.length);
+		System.out.println("small array before: " + Arrays.toString(smallArr));
+		partitioner.partition(smallArr, 0, smallArr.length);
 				
-				System.out.println("repeated array before: " + Arrays.toString(repeatedArr));
-				partitioner.partition(repeatedArr, 0, repeatedArr.length);
+		System.out.println("repeated array before: " + Arrays.toString(repeatedArr));
+		partitioner.partition(repeatedArr, 0, repeatedArr.length);
 				
-				System.out.println("single element array before: " + Arrays.toString(singleElementArr));
-				partitioner.partition(singleElementArr, 0, singleElementArr.length);
-				
-				for(int i = 0; i < 5; i++){
-					String[] randomArr = makeRandomArr(10);
-					System.out.println("random array before: " + Arrays.toString(randomArr));
-					partitioner.partition(randomArr, 0, randomArr.length);
-				}
-	  }
+		System.out.println("single element array before: " + Arrays.toString(singleElementArr));
+		partitioner.partition(singleElementArr, 0, singleElementArr.length);
+			
+		for(int i = 0; i < 5; i++){
+			String[] randomArr = makeRandomArr(10);
+			System.out.println("random array before: " + Arrays.toString(randomArr));
+			partitioner.partition(randomArr, 0, randomArr.length);
+		}
+	}
 }
